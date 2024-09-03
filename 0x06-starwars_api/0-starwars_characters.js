1c87097e31d6f83f25eff1334fdffc539290fa3f
Lahbiyeb Ismail <ismaillhbibe@gmail.com>
2024-09-03 16:02:15 +0100

    script that prints all characters of a Star Wars movie

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -2,46 +2,15 @@
 
 const request = require("request");
 
-if (process.argv.length !== 3) {
-	console.error("Usage: node script.js <Movie ID>");
-	process.exit(1);
-}
-
 const movieId = process.argv[2];
-const apiUrl = `https://swapi.dev/api/films/${movieId}/`;
+const apiUrl = `https://swapi-api.alx-tools.com/api/films/${movieId}/`;
 
-request(apiUrl, (error, response, body) => {
-	if (error) {
-		console.error("Error fetching movie data:", error);
-		return;
-	}
-
-	if (response.statusCode !== 200) {
-		console.error(
-			"Failed to fetch movie data. Status code:",
-			response.statusCode,
-		);
-		return;
-	}
-
+request(apiUrl, (_error, _response, body) => {
 	const movieData = JSON.parse(body);
 	const characterUrls = movieData.characters;
 
 	for (const characterUrl of characterUrls) {
-		request(characterUrl, (error, response, body) => {
-			if (error) {
-				console.error("Error fetching character data:", error);
-				return;
-			}
-
-			if (response.statusCode !== 200) {
-				console.error(
-					"Failed to fetch character data. Status code:",
-					response.statusCode,
-				);
-				return;
-			}
-
+		request(characterUrl, (_error, _response, body) => {
 			const characterData = JSON.parse(body);
 			console.log(characterData.name);
 		});

39d690d6061ac80d58b917b5643eca4da6ebd4a4
Lahbiyeb Ismail <ismaillhbibe@gmail.com>
2024-10-02 14:15:42 +0100

    Determines the winner of a prime number game

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -1,99 +1,55 @@
 #!/usr/bin/python3
 
-"""
-Determines the overall winner of a series of prime number games
-"""
+""" Determines the winner of a prime number game between Maria and Ben """
+
+
+def find_primes(num):
+    """
+    Finds all prime numbers up to a given number.
+
+    Args:
+      num (int): The upper limit (inclusive) to find prime numbers.
+
+    Returns:
+      List[int]: A list of prime numbers up to the specified limit.
+    """
+    primes = [True for i in range(num + 1)]
+    p = 2
+    while p * p <= num:
+        if primes[p]:
+            for i in range(p * p, num + 1, p):
+                primes[i] = False
+        p += 1
+    prime_numbers = [p for p in range(2, num + 1) if primes[p]]
+    return prime_numbers
 
 
 def isWinner(x, nums):
     """
-    Determines the overall winner of a series of prime number games.
+    Determines the winner of a prime number game between Maria and Ben.
 
-      x (int): The number of rounds to be played.
-      nums (list of int): A list of integers where each integer represents
-                the upper limit of the range of numbers (1 to n)
-                to be used in each game.
+    Parameters:
+    x (int): The number of rounds.
+    nums (list of int): List of integers representing the upper limit
+    for each round.
 
-      str: The name of the overall winner ("Maria" or "Ben"). If there is no
-         winner (i.e., both players win an equal number of games), returns
-         None.
-
-    The game rules are as follows:
-    - Two players, Maria and Ben, take turns choosing a prime number from the
-      remaining list of numbers from 1 to n.
-    - After choosing a prime number, all multiples of that prime number are
-      removed from the list.
-    - The player who cannot make a move loses the game.
-    - The function simulates x rounds of the game and determines the overall
-      winner based on the number of games won by each player.
+    Returns:
+    str: The name of the player with the most wins ("Maria" or "Ben").
+       Returns None if there is a tie or if the input is invalid.
     """
-
-    def is_prime(num):
-        """
-        Determine if a given number is a prime number.
-
-        Args:
-          num (int): The number to check for primality.
-
-        Returns:
-          bool: True if the number is prime, False otherwise.
-
-        A prime number is a natural number greater than 1 that
-        has no positive divisors other than 1 and itself.
-        """
-        if num <= 1:
-            return False
-        for i in range(2, int(num**0.5) + 1):
-            if num % i == 0:
-                return False
-        return True
-
-    def play_game(n):
-        """
-        Simulates a game where two players, Maria and Ben,
-        take turns removing numbers from a list of integers from 1 to n.
-        The player who cannot make a move loses the game.
-
-        The rules are as follows:
-        - Players take turns choosing a prime number from the remaining list.
-        - After choosing a prime number, all multiples of that prime number
-        are removed from the list.
-        - The game continues until no prime numbers are left in the list.
-
-        Args:
-          n (int): The upper limit of the range of numbers (1 to n) to be
-          used in the game.
-
-        Returns:
-          int: The winner of the game. 0 if Maria wins, 1 if Ben wins.
-        """
-        remaining = list(range(1, n + 1))
-        turn = 0  # 0 for Maria, 1 for Ben
-        while True:
-            prime_found = False
-            for num in remaining:
-                if is_prime(num):
-                    prime_found = True
-                    prime = num
-                    break
-            if not prime_found:
-                return 1 - turn  # The player who cannot move loses
-            remaining = [num for num in remaining if num % prime != 0]
-            turn = 1 - turn
-
-    maria_wins = 0
-    ben_wins = 0
-
-    for n in nums:
-        winner = play_game(n)
-        if winner == 0:
-            maria_wins += 1
+    if not x or not nums:
+        return None
+    Maria = 0
+    Ben = 0
+    for i in range(x):
+        primes = find_primes(nums[i])
+        if len(primes) % 2 == 0:
+            Ben += 1
         else:
-            ben_wins += 1
-
-    if maria_wins > ben_wins:
+            Maria += 1
+    if Maria > Ben:
         return "Maria"
-    elif ben_wins > maria_wins:
+    elif Ben > Maria:
         return "Ben"
     else:
         return None
